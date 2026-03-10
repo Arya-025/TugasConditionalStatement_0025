ef92a1681e0f41dfab300d5b70e557961915f825
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-03-10 16:26:55 +0700

    membuat fungsi kategori bmi

--- bmi.cpp
+++ bmi.cpp
@@ -16,3 +16,18 @@ float hitungBMI(float berat, float tinggi){
     return berat / (tinggi * tinggi);
 }
 
+// Fungsi menentukan kondisi berat badan
+string kondisiBMI(float bmi){
+    if(bmi < 18.5){
+        return "Berat Badan Kurang";
+    }
+    else if(bmi < 25){
+        return "Berat Badan Normal";
+    }
+    else if(bmi < 30){
+        return "Berat Badan Kelebihan";
+    }
+    else{
+        return "Obesitas";
+    }
+}
