d54d552779b728bf802917541d46f6b2302d66e1
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-03-10 16:25:14 +0700

    membuat prosedur input

--- bmi.cpp
+++ bmi.cpp
@@ -2,3 +2,11 @@
 #include <string>
 using namespace std;
 
+// Prosedur untuk input data
+void inputData(float &berat, float &tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
